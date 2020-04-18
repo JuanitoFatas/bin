1ffcac3d771d7c2306af406147dd529c89327251
Juanito Fatas <katehuang0320@gmail.com>
2020-04-18 22:35:39 +0900

    ohmyzsh, sublime, zshrc, Pryrc

---
+++ sys/apps.sh
@@ -0,0 +1,12 @@
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Sublime
+if [ ! -d "$HOME/bin/" ]; then
+  mkdir "$HOME/bin"
+fi
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "$HOME/bin/subl"
+cp "./sublime_snippets/*" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
+
+cp ./.zshrc "$HOME/.zshrc"
+cp ./.pryrc "$HOME/.pryrc"
